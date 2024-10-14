954d98387a05b94d4121abc87577d02bf9554e00
bugracdnc <bugracaliskandinc@gmail.com>
2024-10-14 22:23:06 +0300

    Functionalize

--- main.go
+++ main.go
@@ -5,8 +5,42 @@ import (
 	"os"
 	"os/exec"
 
+	"atomicgo.dev/cursor"
 	"github.com/fatih/color"
 )
+
+var fggr = color.New(color.FgGreen)
+var bggr = color.New(color.BgGreen, color.FgBlack)
+
+var main_menu_headers = [2]string{"LOGISTIK BIRSEYLER", "BIRSEY MENU"}
+var main_menu_items = [12]string{"1.TESLIM ALMA", "2.TRANSFER", "3.GÖREVLER", "4.SAYIM MENU", "5.KONTROLLER", "6.SORTING", "7.SHIP EVRAK\n", "8.ETIKETLER", "9.SHIP MENU", "0.E-MENU", "SEÇİM: ", "________________"}
+
+func main_menu() string {
+	valid_inputs := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	for {
+		//Print headers
+		bggr.Println(main_menu_headers[0])
+		fmt.Print("   ")
+		bggr.Println(main_menu_headers[1])
+
+		//Print items
+		for _, item := range main_menu_items {
+			fggr.Println(item)
+		}
+		cursor.Up(2)
+		fggr.Print(main_menu_items[len(main_menu_items)-2])
+
+		exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+
+		var b []byte = make([]byte, 1)
+		os.Stdin.Read(b)
+		for _, validator := range valid_inputs {
+			if string(b) == validator {
+				return validator
+			}
+		}
+	}
+}
 
 func main() {
 	//Clear the screen
@@ -14,36 +48,5 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 
-	//Define colors
-	fggr := color.New(color.FgGreen)
-	bggr := color.New(color.BgGreen, color.FgBlack)
-
-	bggr.Println("LOJISTIK BIRSEYLER")
-
-	fmt.Print("   ")
-	bggr.Println("BIRSEY MENU")
-
-	fggr.Println("1.TESLIM ALMA")
-	fggr.Println("2.TRANSFER")
-	fggr.Println("3.GÖREVLER")
-	fggr.Println("4.SAYIM MENU")
-	fggr.Println("5.KONTROLLER")
-	fggr.Println("6.SORTING")
-	fggr.Println("7.SHIP EVRAK")
-	fmt.Println()
-	fggr.Println("8.ETIKETLER")
-	fggr.Println("9.SHIP MENU")
-	fggr.Println("0.E-MENU")
-	fggr.Println("SEÇİM:")
-	fggr.Println("________________")
-
-	fggr.Print("\033[2ASEÇİM: ")
-
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-
-	var b []byte = make([]byte, 1)
-	os.Stdin.Read(b)
-
-	fmt.Println()
-	fmt.Println()
+	main_menu()
 }
