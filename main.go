a1a165f1c522f0aaac47455b108278d879d7c055
bugracdnc <bugracaliskandinc@gmail.com>
2024-10-14 22:28:06 +0300

    Fixed clear screen

--- main.go
+++ main.go
@@ -15,9 +15,12 @@ var bggr = color.New(color.BgGreen, color.FgBlack)
 var main_menu_headers = [2]string{"LOGISTIK BIRSEYLER", "BIRSEY MENU"}
 var main_menu_items = [12]string{"1.TESLIM ALMA", "2.TRANSFER", "3.GÖREVLER", "4.SAYIM MENU", "5.KONTROLLER", "6.SORTING", "7.SHIP EVRAK\n", "8.ETIKETLER", "9.SHIP MENU", "0.E-MENU", "SEÇİM: ", "________________"}
 
+var clear_screen = exec.Command("clear")
+
 func main_menu() string {
 	valid_inputs := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	for {
+		clear_screen.Run()
 		//Print headers
 		bggr.Println(main_menu_headers[0])
 		fmt.Print("   ")
@@ -44,9 +47,8 @@ func main_menu() string {
 
 func main() {
 	//Clear the screen
-	cmd := exec.Command("clear")
-	cmd.Stdout = os.Stdout
-	cmd.Run()
+	clear_screen.Stdout = os.Stdout
+	clear_screen.Run()
 
 	main_menu()
 }
